840fa32e8efc8ad1bbfcf382c0c29a527588d53e
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2021-02-09 16:24:58 -0800

    fix a few things to make it work well with Q

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App, {foo} from './App.svelte';
 window.models = [];
 window.views = [];
 
+window.document.addEventListener("wheel", evt => evt.preventDefault(), { passive: false, capture: false });
+
 let app = new App({
   target: document.body,
   props: {}
